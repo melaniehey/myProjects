1bc8bf6f9446ef739afa669cb772f33fc9e89f29
melaniehey <melanie-kirchkesner@gmx.de>
2023-08-21 18:25:02 +0200

    paare können fast gebildet werden

--- memory/memoryscript.js
+++ memory/memoryscript.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
     //Klammern und Punkte um "document.querySelectorAll('.card')" machen diese Nodelist zum Array
     let cards = [...document.querySelectorAll('.card')];
+    let pairNames = ['pair1', 'pair2', 'pair3', 'pair4', 'pair5', 'pair6'];
     let clicked = false;
     let firstCardClicked = false;
     let secondCardClicked = false;
@@ -61,36 +62,66 @@ window.addEventListener('load', function () {
     }
 
     function buildingPairs() {
-        for (let i = 0; i < 6; i++) {
-            let firstInPair = Math.floor(Math.random() * 6);
-            if (cards[firstInPair].classList.contains('inPair')) {
-                //dann brauchen wir eine neue firstInPair oder?
-                console.log('ToDo: neue firstInPair');
-            } else {
-                cards[firstInPair].classList.add('inPair');
-                console.log(cards[firstInPair]);
+        //ein random pairName wird ausgesucht
+        for (let i = 0; i < pairNames.length; i++) {
+            let randomPairName = Math.floor(Math.random() * pairNames.length);
+            
+            //zwei random karten bekommen die gleiche klasse
+            for (let i = 0; i < 2; i++) {
+                let randomCard = Math.floor(Math.random() * cards.length);
+                cards[randomCard].classList.add('inPair' + randomPairName);
+                console.log(randomPairName, pairNames[randomPairName]);
+                console.log(cards[randomCard]); 
+                
             }
-
-
-            let secondInPair = Math.floor(Math.random() * cards.length);
-            if (cards[secondInPair].classList.contains('inPair')) {
-                console.log('ToDo: neumachen 2');
-            } else {
-                cards[secondInPair].classList.add('inPair');
-                console.log(cards[secondInPair]);
-            }
-
-            if (firstInPair == secondInPair) {
-                buildingPairs();
-                console.log('aksdsbfhsjakaskmjnbmkd');
-            } else {
-                console.log('else');
-            }
-
-
-            // let pair = [firstInPair, secondInPair]
-            // console.log(pair);
+            // console.log(pairNames);
+            pairNames.splice(randomPairName, 1)
+            // console.log(randomPairName);
+            console.log(pairNames);
         }
+
+        //oder
+        //random pairname auswählen und dann aus dem array entfernen.
+        //dann ncohmal das selbe, aber mit den anderen 6 karten(6-11) die keine klasse haben
+
+
+
+
+
+
+
+        // for (let i = 0; i < 6; i++) {
+        //     let firstInPair = Math.floor(Math.random() * 6);
+        //     if (cards[firstInPair].classList.contains('inPair')) {
+        //         //dann brauchen wir eine neue firstInPair oder?
+        //         console.log('ToDo: neue firstInPair');
+        //     } else {
+        //         cards[firstInPair].classList.add('inPair');
+        //         console.log(cards[firstInPair]);
+        //     }
+
+
+        //     let secondInPair = Math.floor(Math.random() * cards.length);
+        //     if (cards[secondInPair].classList.contains('inPair')) {
+        //         console.log('ToDo: neumachen 2');
+        //     } else {
+        //         cards[secondInPair].classList.add('inPair');
+        //         console.log(cards[secondInPair]);
+        //     }
+
+        //     if (firstInPair == secondInPair) {
+        //         buildingPairs();
+        //         console.log('aksdsbfhsjakaskmjnbmkd');
+        //     } else {
+        //         console.log('else');
+        //     }
+        //     // let pair = [firstInPair, secondInPair]
+        //     // console.log(pair);
+        // }
+        //karte randomisen im array cards -> randome zahl für index meiner (karten 17 bekommt pair 1, 5 bekommt auch pair 1)
+        //besser: pärchenname randomisen statt index
+        //eins nach dem anderen bekommt pair1, pair2
+        // get elements by classname
     }
 
     // Timer 
